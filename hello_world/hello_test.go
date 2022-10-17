4fb0624821c64299c017bd9514d6392edc9405fa
Wang, Shensen <swang899@gatech.edu>
2022-10-16 17:36:26 -0700

    update with new test

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	t.Run("should return name if passed in", func(t *testing.T) {
+		got := Hello("Shensen")
+		want := "Hello, Shensen"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("should respond with default if empty string supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
