d7061d4023c49885a4ba2a409b959972b3f30610
Wang, Shensen <swang899@gatech.edu>
2022-10-18 17:52:28 -0700

    init

---
+++ integers/adder_test.go
@@ -0,0 +1,11 @@
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 4)
+	expected := 6
+	if sum != expected {
+		t.Errorf("Expected '%d' but got '%d'", expected, sum)
+	}
+}
