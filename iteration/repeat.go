b3e6be368083d978c7094de70597157b17272126
Shensen Wang <shendude@hotmail.com>
2022-12-18 15:32:53 -0800

    finished iterator

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(input string) string {
+func Repeat(input string, count int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < count; i++ {
 		repeated = repeated + input
 	}
 	return repeated
